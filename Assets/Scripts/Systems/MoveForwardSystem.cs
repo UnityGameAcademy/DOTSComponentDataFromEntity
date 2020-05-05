bd9b3c9cff2e03e56d2d1a3646e47d1f676c03ad
Wilmer Lin <unitygameacademy@gmail.com>
2020-05-05 15:43:04 -0700

    starter project

---
+++ Assets/Scripts/Systems/MoveForwardSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class MoveForwardSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+
+        Entities.
+            WithAll<AsteroidTag>().
+            WithNone<PlayerTag>().
+            ForEach((ref Translation pos, in MoveData moveData, in Rotation rot) =>
+        {
+            float3 forwardDirection = math.forward(rot.Value);
+            pos.Value += forwardDirection * moveData.speed * deltaTime;
+
+        }).ScheduleParallel();
+
+
+
+    }
+}
