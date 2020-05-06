d1febae9d018021de75d4ddf8db6fcfcb575460c
Wilmer Lin <unitygameacademy@gmail.com>
2020-05-05 22:55:41 -0700

    modifying FaceDirection

--- Assets/Scripts/Systems/FaceDirectionSystem.cs
+++ Assets/Scripts/Systems/FaceDirectionSystem.cs
@@ -6,14 +6,36 @@ public class FaceDirectionSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        Entities.ForEach((ref Rotation rot, in Translation pos, in MoveData moveData) =>
-        {
-            if (!moveData.direction.Equals(float3.zero))
+        Entities.
+            WithAll<PlayerTag>().
+            ForEach((ref Rotation rot, in Translation pos, in MoveData moveData) =>
             {
-                quaternion targetRotation = quaternion.LookRotationSafe(moveData.direction, math.up());
-                rot.Value = math.slerp(rot.Value, targetRotation, moveData.turnSpeed);
-            }
-        }).Schedule();
+                FaceDirection(ref rot, moveData);
 
+            }).Schedule();
+
+        Entities.
+            WithNone<PlayerTag>().
+            WithAll<ChaserTag>().
+            ForEach((ref MoveData moveData, ref Rotation rot, in Translation pos, in TargetData targetData) =>
+            {
+                ComponentDataFromEntity<Translation> allTranslations = GetComponentDataFromEntity<Translation>(true);
+                Translation targetPos = allTranslations[targetData.targetEntity];
+
+                float3 dirToTarget = targetPos.Value - pos.Value;
+                moveData.direction = dirToTarget;
+                FaceDirection(ref rot, moveData);
+
+            }).Run();
+
+    }
+
+    private static void FaceDirection(ref Rotation rot, MoveData moveData)
+    {
+        if (!moveData.direction.Equals(float3.zero))
+        {
+            quaternion targetRotation = quaternion.LookRotationSafe(moveData.direction, math.up());
+            rot.Value = math.slerp(rot.Value, targetRotation, moveData.turnSpeed);
+        }
     }
 }
